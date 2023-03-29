13e0128b9d1959e987079c41021eca9e4ada98cf
RebellionR2 <88272981+RebellionR2@users.noreply.github.com>
2023-03-30 03:09:07 +0530

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,28 @@
+let result=document.getElementById("result");
+
+function insertValue(value){
+    result.value+=value;
+}
+
+function clearResult()
+{
+    result.value="";
+}
+
+function clearLastChar()
+{
+    result.value=result.value.slice(0,-1);
+}
+
+
+function calculate()
+{
+  try{
+    result.value=eval(result.value);
+  }
+  catch(error)
+  {
+    result.value="Error";
+  }
+
+}
